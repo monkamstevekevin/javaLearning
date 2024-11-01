060719520382fd03643ce27cdfac93a3330bcf8b
monkamsteve <janesteve237@gmail.com>
2024-11-01 17:29:51 -0400

    making a generic list class to handle all the generic list i want to build

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import exceptions.ExceptionDemo;
+import generics.GenericList;
 import generics.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-   var list  =  new List();
-   list.add(10);
+ var list  =  new GenericList<Integer>();
+ list.add(1);
+ list.add(2);
+  int number =  list.get(2);
 
     }
 }
---
+++ src/generics/GenericList.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class GenericList<T>{
+     private T[] elements   = (T[]) new Object[10];;
+     private int size;
+
+     public void add(T element) {
+         elements[size++] = element;
+
+     }
+     public T get(int index) {
+         return elements[index];
+     }
+
+}
